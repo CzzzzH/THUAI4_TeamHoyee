beba45083ce8af04127ddadcba4b0a409773305f
BowmanChow <bowmanchow@gmail.com>
2021-04-21 04:18:16 +0000

    dijkstra

--- src/player.cpp
+++ src/player.cpp
@@ -7,11 +7,222 @@ extern const bool asynchronous = true;
 #include <random>
 #include <iostream>
 #include <unordered_map>
-#include "polypartition.h"
-#include "PolygonConvexPartition.h"
-#include "map_poly.h"
+// #include "polypartition.h"
+// #include "PolygonConvexPartition.h"
+// #include "map_poly.h"
 #include "math.h"
 #include <unistd.h>
+#include <iostream>
+#include <ext/pb_ds/priority_queue.hpp>
+#include <unordered_set>
+// #include "MainWindow.h"
+#include <math.h>
+#include <stdlib.h>
+#include <ctime>
+#include <memory>
+#include <list>
+#include <string.h>
+
+const int LENGTH = 50;
+
+static unsigned char defaultMap[LENGTH][LENGTH] = {
+	//0                             10                            20                            30                            40                         49
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, //0
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0, 1},
+	{1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //10
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //20
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //30
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}, //40
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 8, 0, 1},
+	{1, 0, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 0, 0, 1},
+	{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+	{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1} //49
+};
+const int ZOOM = 10;
+
+std::array<int, 2> operator+(const std::array<int, 2> &p1, const std::array<int, 2> &p2)
+{
+	return std::array<int, 2>{p1[0] + p2[0], p1[1] + p2[1]};
+}
+std::array<int, 2> operator-(const std::array<int, 2> &p1, const std::array<int, 2> &p2)
+{
+	return std::array<int, 2>{p1[0] - p2[0], p1[1] - p2[1]};
+}
+std::array<int, 2> operator-(const std::array<int, 2> &p1)
+{
+	return std::array<int, 2>{-p1[0], -p1[1]};
+}
+
+bool operator==(const std::array<int, 2UL> &p1, const std::array<int, 2UL> &p2)
+{
+	return p1[0] == p2[0] && p1[1] == p2[1];
+}
+double length(const std::array<int, 2> &p1)
+{
+	return sqrt(p1[0] * p1[0] + p1[1] * p1[1]);
+}
+
+static unsigned char route[LENGTH][LENGTH];
+static char colorMap[LENGTH][LENGTH];
+static char colorValueMap[LENGTH][LENGTH];
+
+const std::array<int, 2> operate[] = {
+	{1, 0},
+	{1, 1},
+	{0, 1},
+	{-1, 1},
+	{-1, 0},
+	{-1, -1},
+	{0, -1},
+	{1, -1}};
+struct frontier_node
+{
+	double distance;
+	std::array<int, 2> position;
+	bool operator>(const frontier_node &n) const
+	{
+		return distance > n.distance;
+	}
+};
+void dijkstra(const std::array<int, 2> &point)
+{
+	memset(route, 0, sizeof(route));
+	memset(colorValueMap, 0, sizeof(colorValueMap));
+	// std::cout << sizeof(route) << std::endl;
+	__gnu_pbds::priority_queue<frontier_node, std::greater<frontier_node>> frontier;
+	double distance_table[LENGTH][LENGTH];
+	memset(distance_table, -1, sizeof(distance_table));
+	bool dont_search[LENGTH][LENGTH];
+	memset(dont_search, 0, sizeof(dont_search));
+
+	frontier.push(frontier_node{0, point});
+	distance_table[point[0]][point[1]] = 0;
+	colorValueMap[point[0]][point[1]] = colorMap[point[0]][point[1]];
+	while (1)
+	{
+		if (frontier.empty())
+		{
+			std::cout << "frontier is empty" << std::endl;
+			break;
+		}
+		auto searching_point = frontier.top().position;
+		frontier.pop();
+		// std::cout << "searching :  (" << searching_point[0] << "," << searching_point[1] << ")" << std::endl;
+		auto distance = distance_table[searching_point[0]][searching_point[1]];
+		auto color = colorValueMap[searching_point[0]][searching_point[1]];
+		// std::cout << "distance :  " << distance << std::endl;
+		dont_search[searching_point[0]][searching_point[1]] = 1;
+		bool avaiable[8];
+		memset(avaiable, 1, sizeof(avaiable));
+		for (int i = 0; i < 8; i += 2)
+		{
+			auto p = searching_point + operate[i];
+			if (defaultMap[p[0]][p[1]])
+			{
+				// std::cout << "setting unavaiable : " << i << std::endl;
+				avaiable[i] = 0;
+				avaiable[(i + 8 - 1) % 8] = 0;
+				avaiable[(i + 1) % 8] = 0;
+			}
+		}
+		for (int i = 1; i < 8; i += 2)
+		{
+			auto p = searching_point + operate[i];
+			if (defaultMap[p[0]][p[1]])
+			{
+				// std::cout << "setting unavaiable : " << i << std::endl;
+				avaiable[i] = 0;
+			}
+		}
+		for (int i = 0; i < 8; ++i)
+		{
+			if (!avaiable[i])
+				continue;
+			auto p = searching_point + operate[i];
+			// std::cout << "updating children :  (" << p[0] << "," << p[1] << ")" << std::endl;
+			double neighbor_distance = i % 2 ? sqrt(2) : 1;
+			if (dont_search[p[0]][p[1]])
+				continue;
+			if (distance_table[p[0]][p[1]] >= 0 && distance + neighbor_distance > distance_table[p[0]][p[1]])
+				continue;
+			auto iter = frontier.begin();
+			for (; iter != frontier.end(); iter++)
+			{
+				if (iter->position == p)
+					break;
+			}
+			// std::cout << "updating distance : " << distance + neighbor_distance << std::endl;
+			if (iter == frontier.end())
+				frontier.push(frontier_node{distance + neighbor_distance, p});
+			else
+				frontier.modify(iter, frontier_node{distance + neighbor_distance, p});
+			distance_table[p[0]][p[1]] = distance + neighbor_distance;
+			colorValueMap[p[0]][p[1]] = color + colorMap[p[0]][p[1]];
+			route[p[0]][p[1]] = i;
+			// std::cout << "complete a child  " << std::endl;
+		}
+	}
+}
+std::list<unsigned char> searchWayFromMap(
+	std::array<int, 2> start, std::array<int, 2> end,
+	std::function<void(std::array<int, 2>)> func = [](std::array<int, 2> p) {})
+{
+	std::list<unsigned char> result;
+	while (1)
+	{
+		func(end);
+		if (end == start)
+			break;
+		result.push_front(route[end[0]][end[1]]);
+		end = end - operate[route[end[0]][end[1]]];
+		// std::cout << "(" << end[0] << "," << end[1] << ")  :  " << (int)colorMap[end[0]][end[1]] << std::endl;
+	}
+	return result;
+}
 
 /* 请于 VS2019 项目属性中开启 C++17 标准：/std:c++17 */
 
@@ -22,64 +233,18 @@ namespace
 	[[maybe_unused]] std::uniform_real_distribution<double> direction(0, 2 * 3.1415926);
 	[[maybe_unused]] std::default_random_engine e{std::random_device{}()};
 }
-
-TPPLPartition pp;
-list<TPPLPoly> testpolys, result;
-std::unordered_multimap<TPPLPoint, TPPLPolyList::iterator, hash_point> point_poly_map;
-
-bool is_map_done = false;
-TPPLPoint target = TPPLPoint{25, 25};
-
-int count_i = 0;
-
+std::array<int, 2> target = {40, 25};
 void AI::play(GameApi &g)
 {
 	auto self = g.GetSelfInfo();
-
-	if (!is_map_done)
-	{
-		auto p_list = map_poly;
-		for (auto l1 = p_list.begin(); l1 != p_list.end(); ++l1)
-		{
-			TPPLPoly poly;
-			poly.Init();
-			if (l1 != p_list.begin())
-				poly.SetHole(true);
-			// int i = 0;
-			for (auto p : *l1)
-			{
-				poly.push_back({p[0], p[1]});
-			}
-			testpolys.push_back(poly);
-		}
-		pp.ConvexPartition_HM(&testpolys, &result, point_poly_map);
-		is_map_done = true;
-		for (auto poly_iter = result.begin(); poly_iter != result.end(); poly_iter++)
-		{
-			std::cout << &*poly_iter << "  :  ";
-			auto k = poly_iter->begin();
-			for (; k != --poly_iter->end(); ++k)
-			{
-				std::cout << "(" << k->x << "," << k->y << ")   ";
-				auto tmp = k;
-				++tmp;
-			}
-			std::cout << "(" << k->x << "," << k->y << ")   " << std::endl;
-		}
-	}
-
-	auto way_list = searchWay(result, point_poly_map, target, TPPLPoint{double(self->x), double(self->y)} / 1000, 2.5);
 
 	if (self->bulletNum)
 	{
 		g.Attack(100, 0);
 	}
-	for (auto w : way_list)
-	{
-		std::cout << "(" << w.x << "," << w.y << ")";
-	}
-	std::cout << std::endl;
-	double angle = atan2((++way_list.begin())->y - (self->y / 1000), (++way_list.begin())->x - (self->x / 1000));
+	dijkstra({(int)self->x / 1000, (int)self->y / 1000});
+	auto l = searchWayFromMap({(int)self->x / 1000, (int)self->y / 1000}, target);
+	double angle = *l.begin() * M_PI / 4;
 	std::cout << "angle : " << angle << "\n";
 	g.MovePlayer(50, angle);
 	// g.MovePlayer(50, 1);
